5ec74209748fe1f59c95a2afd9da505205ae7ae1
Алексей <52876090+AlexeyVodopyanov@users.noreply.github.com>
2020-10-24 15:22:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+namespace a1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BackgroundColor = ConsoleColor.Blue;
+            ForegroundColor = ConsoleColor.White;
+            WriteLine("Привет");
+            WriteLine("Давай посчитаешь математика");
+            ResetColor();
+            int a;
+            Write("12 * 12 = ");
+            a = Convert.ToInt32(ReadLine());
+            if (a == 144)
+            {
+                BackgroundColor = ConsoleColor.Green;
+                ForegroundColor = ConsoleColor.White;
+                WriteLine("Верно!");
+                ResetColor();
+            }
+            else
+            {
+                BackgroundColor = ConsoleColor.Red;
+                ForegroundColor = ConsoleColor.White;
+                WriteLine("Не верно!");
+                ResetColor();
+            }
+        }
+    }
+}
